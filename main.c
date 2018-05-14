43bc5d6fda874db43be9040989edd9ed5fce7f95
Marcos Delgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-05-14 20:43:32 -0300

    Punteros

--- main.c
+++ main.c
@@ -1,44 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct
-{
-    int dia;
-    int mes;
-    int anio;
-
-}eFecha;
-
-typedef struct
-{
-    int legajo;
-    char nombre[20];
-    char sexo;
-    float sueldo;
-    eFecha fechaIngreso;
-
-}eEmpleado;
-
-void mostrarEmpleado(eEmpleado);
-
-eEmpleado conseguirEmpleado();
-
-
 int main()
 {
-    eEmpleado plantel[] = {{ 1234, "Marcos", 'm', 30000.00,{23,10,2000} }, { 1111, "Lucas", 'm', 20000.00,{22,11,2003} }, { 2222, "John", 'm', 10000.00,{20,9,2010} }};
+    int vec[5];
 
 
-   /* eEmpleado otroEmpleado;
-    unEmpleado = conseguirEmpleado();
-    mostrarEmpleado(unEmpleado);*/
 
+
+
+
+    printf("%d", *(vec+4));
     return 0;
 }
-
-
-
-
-
-
