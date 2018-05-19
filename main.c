56ba85d43a84fa48376e64976012cd03ae207c44
Marcos Delgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-05-18 22:10:50 -0300

    punteros

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+    int idTipo;
+    char descripcion[20];
+}eTipo;
+
+typedef struct{
+    int id;
+    char nombre[20];
+    int edad;
+    eTipo tipo;
+}eMascota;
+
+
 int main()
 {
-    int vec[5];
+  eMascota unaMascota;
+  unaMascota.edad=5;
+  unaMascota.id=1234;
+  strcpy(unaMascota.nombre, "colita");
 
+  modficarEdad(&unaMascota, 20);
+  printf("%d %s %d\n\n", unaMascota.id, unaMascota.nombre, unaMascota.edad);
 
+  return 0;
+}
+void modficarEdad(eMascota*x, int edad){
 
-
-
-
-    printf("%d", *(vec+4));
-    return 0;
+    x->edad=edad;
 }
